2a609fdca50722bcf6c2d3229a17aa98eb74cf82
Ameerat10 <a.ademuyiwa@alustudent.com>
2024-07-05 03:17:33 +0000

    "changed the height of the bg image"

--- frontend/zaabuni/src/App.jsx
+++ frontend/zaabuni/src/App.jsx
@@ -1,13 +1,17 @@
-//import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Hero from './components/Hero';
 
 function App() {
 
   return (
-    <div className='font-ubuntu'>
-      
-      {/* <Navbar /> */}
+    <div className='font-ubuntu bg-[url(./assets/zaablack.png)] bg-no-repeat bg-cover bg-center lg:bg-top h-96 lg:h-[450px] w-full'>
+      <div className='backdrop-brightness-75 bg-black/50 h-full w-full'>
+      <Navbar />
+      <Hero />
+      </div>
       <Home />
+     
       
     </div>
   )
--- frontend/zaabuni/src/components/Home.jsx
+++ frontend/zaabuni/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import About from './About'
 import Contact from './Contact'
-import Hero from './Hero';
+//import Hero from './Hero';
 
 const Home = () => {
   return (
     <div name="home" className="py-16px-4 font-play">
-      <Hero />
+      {/* <Hero /> */}
       <About />
       <Contact />
     </div>

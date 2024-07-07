6414468676e0e4cee1cd37fdfc28e14b641712aa
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-07-07 02:49:53 +0000

    "enhanced donation page"

--- frontend/zaabuni/src/components/Donate.jsx
+++ frontend/zaabuni/src/components/Donate.jsx
@@ -8,11 +8,12 @@ const Donation = () => {
       <div className="containerg mx-4">
         <h2 className="text-3xl font-bold pt-5 text-centerg text-yellow-950">Make a Donation</h2>
         <div className='grid lg:grid-cols-2 lg:p-16'>
-        <img src={zaabdonate} alt="" className='lg:h-80' />
+        {/* <img src={zaabdonate} alt="" className='lg:h-80' /> */}
           <p className="mb-4 text-xl text-gray-700">
             Your generous contributions help us to support and promote local development, build human capital, attract investment, and skilled personnel to the area. Thank you for your support!
           </p>
           </div>
+          <div className='bg-gray-200 shadow rounded-md py-10'>
           {/* <h3 className="text-3xl text-center font-semibold mb-2 text-yellow-950">Bank Details</h3> */}
           <p className="mb-8 grid ml-24">
             <FontAwesomeIcon icon={faUniversity} className="text-[#00df9a] ml-8 mb-3 flex h-10" />
@@ -37,7 +38,8 @@ const Donation = () => {
             <FontAwesomeIcon icon={faCreditCard} className="text-blue-600 mr-2" />
             <strong>IBAN:</strong> XYZ12345678901234567890
           </p> */}
-          <p className="mt-4 text-xl">
+          </div>
+          <p className="mt-4 pb-6 text-xl">
             Please reference your donation with your name or email address so we can acknowledge your contribution. For any queries or to inform us about your donation, please contact us at <a href="mailto:info@zaabuniyouth.org" className="text-yellow-950 hover:underline">
               info@zaabuniyouth.org
             </a>.
